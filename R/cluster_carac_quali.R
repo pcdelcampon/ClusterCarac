e0866732c0370f8a9ff76231bc04ddb9e2be7d4b
Pedro Cesar Del Campo Neira <pcdelcampon@gmail.com>
2025-11-27 15:10:21 -0500

    Add Titanic example to cluster_carac_quali docs

--- R/cluster_carac_quali.R
+++ R/cluster_carac_quali.R
@@ -30,6 +30,20 @@
 #' @author Pedro Cesar Del Campo Neira (\email{pcdelcampon@gmail.com})
 #' @references Lebart, L. and Morineau, A. and Piron, M. (1995) Statisitique exploratoire multidimensionnelle, Paris. 
 #' Pardo C, Del Campo P (2007). "Combinación de métodos factoriales y de análisis de conglomerados en R: el paquete FactoClass." Revista Colombiana de Estadística, 30(2), 231-245.
+#' @examples
+#' # Titanic example (base R dataset)
+#' titanic_df <- as.data.frame(Titanic) |> 
+#'   tidyr::uncount(Freq) |> 
+#'   dplyr::mutate(
+#'     Class = as.factor(Class),
+#'     Sex   = as.factor(Sex),
+#'     Age   = as.factor(Age),
+#'     Survived = as.factor(Survived)
+#'   )
+#' dtf    <- titanic_df |> dplyr::select(Class, Sex, Age)
+#' classc <- titanic_df$Survived
+#' cluster_carac_quali(dtf, classc, alpha = 0.05)
+#' 
 #' @export
 #' @importFrom dplyr count group_by mutate ungroup transmute arrange filter rename select
 #' @importFrom tidyr pivot_longer
